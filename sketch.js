29c8d17eb9d03f957d323a4a11fe72fa3da2330f
Alex <alex@2024vivoilak.emome-ip6.hinet.net>
2024-07-17 14:34:36 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,123 +1,169 @@
 // Alex Donley
-// https://thecodingtrain.com/challenges/36-blobby
 
-let phase = 0.0;
+let phase = 0;
+let zoff = 0;
+let r;
+let c;
+let res = 300
+
+let deformations = [];
+
+let img;
+let snd;
+let sceneAmp;
 let gArray = []
-let song
 
-function setup() {
-  createCanvas(600, 600);
-  // numOfVert = createSlider(2, 80, 40, 1);
-  wavelength = createSlider(2, 40, 10, 0.5);
-  amplitude = createSlider(0, 100, 100, 1);
-  rotation = createSlider(0, 1, 0, .01);
-  interpolation = createSlider(1, 200, 3, 1)
-  phaseSlide = createSlider(-1, 1, 0.5, 0.01)
+function preload() {
+  img = loadImage('img/p5.jpg')
 
   limit = 12
 
   for (i = 0; i<limit; i++) {
     gArray[i] = loadSound('sfx/g' + (i + 1) +'.wav')
   }
+}
+
+function setup() {
+  createCanvas(windowWidth, 600);
+  c = width * 0.75;
+  cordHeight = createSlider(1, 300, 75);
+  displacer = createSlider(50, res-50, res/2);
+  angleMode(DEGREES)  
 
   sceneAmp = new p5.Amplitude();
 }
 
 function draw() {
-  background(0);
-  noFill(); 
-  stroke(255)
   
-  let res = width;
-  //let amp = amplitude.value();
-  let amp = sceneAmp.getLevel() * amplitude.value();
-  // if (gur.isPlaying()) {
-  //     let amp = gur.getLevel();
-  // }
-  
-  let limit = wavelength.value() * PI;
-  let waveArray = [];
-  
-  // ellipse(width/2, width/2 + 50, 5, 5)
-  
-  //raw arrays
-  
-  for (let n=0; n<res+1; n++){
-    let x = n * limit / res
-    let y;
-    if (x < TWO_PI){
-      y = ((cos(x+PI) + 1));
-    } else {
-      y = 0;
-    }
-    waveArray.push([x, y])
-  }
-  
-  translate(0, height)
- 
-  //linear visual
-   
-  // beginShape();
-  // waveArray.forEach((element) =>{
-  //   vertex(element[0]*width/limit, ((element[1]*amp/20)+1)*-1*height/2)
-  // })
-  // endShape();
-  
- 
-  //circular visual
-  
-  ellipse(width/2 + 150, width/2, 5, 5)
-  translate(width / 2, - height / 2);
+  background(255);
+  image(img, 0,0, height, height);
 
-//   var radius = 150;
-//   var rot = rotation.value()*TWO_PI;
+  stroke(0);
+  strokeWeight(5);
+  fill(255, 213, 154);
+  noFill();
 
-//   beginShape();
-//   for (var a = 0; a < res; a += 1) {
-//     let r = radius + (waveArray[a][1] * amp);
-//     let x = r * cos(TWO_PI*a/res + rot);
-//     let y = r * sin(TWO_PI*a/res + rot);
-//     vertex(x, y);
-//   }
-//   endShape(CLOSE);
-  
-  
-//   wiggle interpolation
-//   translate(-width / 2, 0);
-  
-//   beginShape()
-//   for (let m = 0; m < waveArray.length; m++){
-//     let x = m;
-//     let y = cos(m/interpolation.value() + phase) * height * amp * waveArray[m][1]/40;
-//     vertex(x, y);
-//   }
-//   endShape()
-//   translate(width / 2, 0);
-  
-//wiggle circle
-  
-  
-  var radius = height / 4;
-  var rot = rotation.value()*TWO_PI;
+  breath = map(sin(phase * 3), -1, 1, 0, 10)
+  h = 120 //+ wig;
 
-  beginShape();
-  for (var a = 0; a < res; a += 1) {
-    let r = radius + (cos(a/interpolation.value() + phase) * amp * waveArray[a][1]);
-    let x = r * cos(TWO_PI*a/res + rot);
-    let y = r * sin(TWO_PI*a/res + rot);
+  deformations = []
+
+  gur = sceneAmp.getLevel() * 10
+
+  //createSineDisplacement(0, 300, 10, 1, -5)
+  createSineDisplacement(100, 300, 20)
+  createSineDisplacement(0, 300, 5, 1, -5)
+  createSineDisplacement(0, 150, 4 * gur, 2, 100)
+
+  // NEED TO: fix inversion when initial vertex is in greater x / y value than second vertex
+  // NEED TO: fix errors of division by zero when y1 and y2 equal each other
+
+  x1 = 329
+  y1 = 305
+  x2 = 300
+  y2 = 530
+  dir = 1
+
+  circArray = calculateArc(x1, y1, x2, y2, dir * (h + breath));
+  translate(circArray[0], circArray[1])
+
+  arcDiff = circArray[4] - circArray[3]
+
+  beginShape()
+  vertex(x1 - circArray[0], y1 - circArray[1])
+  for (let a = 0; a < res; a += 1){
+    // index = map(a, circArray[3], circArray[4], 0, res-1)
+    // console.log(index)
+    pos = map(a, 0, res, circArray[3], circArray[4])
+
+    rad = circArray[2]
+    newrad = rad
+
+    deformations.forEach((deform) =>{
+      newrad -= deform[a];
+    })
+    
+    let x = newrad * cos(pos);
+    let y = newrad * sin(pos);
+
     vertex(x, y);
   }
-  endShape(CLOSE);
+  vertex(x2 - circArray[0], y2 - circArray[1])
+  endShape()
 
-  phase += phaseSlide.value();
+  translate(-circArray[0], -circArray[1])
+
+
+  phase += 0.5;
+  //noLoop();
 }
 
-function playWav() {
-  // if (!song.isPlaying()) {
-    n = Math.floor(Math.random() * limit)
+function calculateArc(startx, starty, endx, endy, height) {
+  distance = Math.sqrt((endy - starty)**2 + (endx - startx)**2);
+  slope = (endy - starty) / (endx - startx);
+  bigPerpendicular = -1 * slope**-1;
 
-    song = gArray[n]
-    console.log(song.duration())
-    song.play();
-  //}
+  midx = (endx + startx) / 2;
+  midy = (endy + starty) / 2;
+
+  distHalf = distance / 2
+  miniSlope = height / distHalf;
+  smallPerpendicular = -1 * miniSlope**-1
+
+  rad = height / 2 - (smallPerpendicular * distHalf/2)
+  offset = rad - height
+
+  circy = midy + offset * bigPerpendicular / Math.sqrt(1 + bigPerpendicular**2)
+  circx = midx + offset / Math.sqrt(1 + bigPerpendicular**2)
+
+  rotation = 90 - atan(slope)
+  innerAngle = atan(distHalf / offset)
+
+  arcStart = (Math.floor(innerAngle/100) * -1) * 180 + innerAngle - rotation
+  arcEnd = 360 - arcStart - 2 * rotation
+
+  //console.log(distance, slope, bigPerpendicular, midx, midy, rad, offset, circx, circy)
+  //console.log(rotation, innerAngle, arcStart, arcEnd)
+  let dataArray = [circx, circy, rad, arcStart, arcEnd]
+  return dataArray;
+}
+
+function createSineDisplacement(start, end, amp, innerwl, speed){
+  diff = end - start;
+
+  //console.log(diff)
+  sineArray = [];
+  for (i = 0; i < res; i++) {
+    if (i < start || i > end) {
+      sineArray.push(0);
+    } else {
+      pnt = map(i, start, end, 0, 360)
+      wave = cos(pnt)
+      adjusted = map(wave, -1, 1, -1, 0) * amp 
+      if (innerwl) {
+        interp = adjusted * cos(i * innerwl + phase * speed);
+        sineArray.push(interp)
+      } else {
+        sineArray.push(adjusted);
+      }
+      
+    }
+  }
+
+  console.log(cos(360))
+  deformations.push(sineArray);
+
+  return sineArray;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, 400);
+}
+
+function playSound() {
+  n = Math.floor(Math.random() * limit)
+
+  snd = gArray[n]
+  console.log(snd.duration())
+  snd.play();
 }
